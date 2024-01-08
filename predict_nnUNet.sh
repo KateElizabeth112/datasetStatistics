dc47e046ba3899baed6fc2cc7c171e5fd1bdbd3a
Kate Cevora <kate.wilkinson@illumr.com>
2024-01-08 14:07:44 +0000

    Debuggging

--- predict_nnUNet.sh
+++ predict_nnUNet.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#PBS -l walltime=40:00:00
+#PBS -l walltime=2:00:00
 #PBS -l select=1:ncpus=15:mem=120gb:ngpus=1:gpu_type=RTX6000
 #PBS -N predict_TS_sex
 
@@ -23,17 +23,19 @@ DATASET="TotalSegmentator"
 #             "Dataset700_Age2" "Dataset701_Age2" "Dataset702_Age2"
 #             "Dataset800_Age3" "Dataset801_Age3" "Dataset802_Age3"
 #             "Dataset900_Age4" "Dataset901_Age4" "Dataset902_Age4")
-experiments=("Dataset500_Fold0" "Dataset501_Fold0" "Dataset502_Fold0"
-             "Dataset600_Fold1" "Dataset601_Fold1" "Dataset602_Fold1"
-             "Dataset700_Fold2" "Dataset701_Fold2" "Dataset702_Fold2"
-              "Dataset800_Fold3" "Dataset801_Fold3" "Dataset802_Fold3"
-              "Dataset900_Fold4" "Dataset901_Fold4" "Dataset902_Fold4")
+#experiments=("Dataset500_Fold0" "Dataset501_Fold0" "Dataset502_Fold0"
+#             "Dataset600_Fold1" "Dataset601_Fold1" "Dataset602_Fold1"
+#             "Dataset700_Fold2" "Dataset701_Fold2" "Dataset702_Fold2"
+#              "Dataset800_Fold3" "Dataset801_Fold3" "Dataset802_Fold3"
+#              "Dataset900_Fold4" "Dataset901_Fold4" "Dataset902_Fold4")
+
+experiments=("Dataset600_Fold1")
 
 export nnUNet_raw=$ROOT_DIR"nnUNet_raw"
 export nnUNet_preprocessed=$ROOT_DIR"nnUNet_preprocessed"
 export nnUNet_results=$ROOT_DIR"nnUNet_results"
 
-for number in {0..14}; do
+for number in {0..1}; do
     EXPERIMENT=${experiments[number]}
     TASK=${EXPERIMENT:7:3}
 
@@ -50,7 +52,7 @@ for number in {0..14}; do
     #nnUNetv2_predict -i $INPUT_FOLDER -o $OUTPUT_FOLDER -d $TASK -c 3d_fullres -f all -chk checkpoint_best.pth
 
     # Run python script to evaluate results
-    #python3 processResults.py -d $DATASET -e $EXPERIMENT -r $ROOT_DIR
+    python3 processResults.py -d $DATASET -e $EXPERIMENT -r $ROOT_DIR
 
 done
 
