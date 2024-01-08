ea305a50eb133af06e3c5c1fc569655b6cc1bb1e
Kate Cevora <kate.wilkinson@illumr.com>
2024-01-08 10:51:02 +0000

    reduced number of iterations

--- predict_nnUNet.sh
+++ predict_nnUNet.sh
@@ -23,17 +23,17 @@ DATASET="AMOS"
 #             "Dataset700_Age2" "Dataset701_Age2" "Dataset702_Age2"
 #             "Dataset800_Age3" "Dataset801_Age3" "Dataset802_Age3"
 #             "Dataset900_Age4" "Dataset901_Age4" "Dataset902_Age4")
-experiments=("Dataset500_Fold0" "Dataset501_Fold0" "Dataset502_Fold0"
-             "Dataset600_Fold1" "Dataset601_Fold1" "Dataset602_Fold1"
-             "Dataset700_Fold2" "Dataset701_Fold2" "Dataset702_Fold2"
-             "Dataset800_Fold3" "Dataset801_Fold3" "Dataset802_Fold3"
+#experiments=("Dataset500_Fold0" "Dataset501_Fold0" "Dataset502_Fold0"
+#             "Dataset600_Fold1" "Dataset601_Fold1" "Dataset602_Fold1"
+#             "Dataset700_Fold2" "Dataset701_Fold2" "Dataset702_Fold2"
+experiments=("Dataset800_Fold3" "Dataset801_Fold3" "Dataset802_Fold3"
              "Dataset900_Fold4" "Dataset901_Fold4" "Dataset902_Fold4")
 
 export nnUNet_raw=$ROOT_DIR"nnUNet_raw"
 export nnUNet_preprocessed=$ROOT_DIR"nnUNet_preprocessed"
 export nnUNet_results=$ROOT_DIR"nnUNet_results"
 
-for number in {0..14}; do
+for number in {0..5}; do
     EXPERIMENT=${experiments[number]}
     TASK=${EXPERIMENT:7:3}
 
